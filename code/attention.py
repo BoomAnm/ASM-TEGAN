7bf73415dd4dece3923a9e2f2373371b092ab9f1
Hangan <82016703+BoomAnm@users.noreply.github.com>
2024-12-16 20:53:24 +0800

    Add files via upload

---
+++ code/attention.py
@@ -0,0 +1,170 @@
+import torch
+import torch.nn as nn
+from einops.layers.torch import Rearrange
+
+def conv1x1(in_planes, out_planes):
+    "1x1 convolution with padding"
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1,
+                     padding=0, bias=False)
+
+
+def func_attention(query, context, gamma1):
+    batch_size, queryL = query.size(0), query.size(2)
+    ih, iw = context.size(2), context.size(3)
+    sourceL = ih * iw
+
+    context = context.view(batch_size, -1, sourceL)
+    contextT = torch.transpose(context, 1, 2).contiguous()
+
+    attn = torch.bmm(contextT, query)
+    attn = attn.view(batch_size * sourceL, queryL)
+    attn = nn.Softmax()(attn)
+    attn = attn.view(batch_size, sourceL, queryL)
+    attn = torch.transpose(attn, 1, 2).contiguous()
+    attn = attn.view(batch_size * queryL, sourceL)
+    attn = attn * gamma1
+    attn = nn.Softmax()(attn)
+    attn = attn.view(batch_size, queryL, sourceL)
+    attnT = torch.transpose(attn, 1, 2).contiguous()
+
+    weightedContext = torch.bmm(context, attnT)
+
+    return weightedContext, attn.view(batch_size, -1, ih, iw)
+
+
+class SpatialAttention(nn.Module):
+    def __init__(self, idf, cdf):
+        super(SpatialAttention, self).__init__()
+        self.conv_context = conv1x1(cdf, idf)
+        self.sm = nn.Softmax()
+        self.mask = None
+        self.idf = idf
+
+    def applyMask(self, mask):
+        self.mask = mask
+
+    def forward(self, input, context):
+        ih, iw = input.size(2), input.size(3)
+        queryL = ih * iw
+        batch_size, sourceL = context.size(0), context.size(2)
+
+        target = input.view(batch_size, -1, queryL)
+        targetT = torch.transpose(target, 1, 2).contiguous()
+        sourceT = context.unsqueeze(3)
+
+        sourceT = self.conv_context(sourceT).squeeze(3)
+
+        attn = torch.bmm(targetT, sourceT)
+        attn = attn.view(batch_size * queryL, sourceL)
+        if self.mask is not None:
+            mask = self.mask.repeat(queryL, 1)
+            attn.data.masked_fill_(mask.data, -float('inf'))
+
+        attn = self.sm(attn)
+        attn = attn.view(batch_size, queryL, sourceL)
+        attn = torch.transpose(attn, 1, 2).contiguous()
+
+        weightedContext = torch.bmm(sourceT, attn)
+        attn = attn.view(batch_size, -1, ih, iw)
+
+        return weightedContext, attn
+
+
+class ChannelAttention(nn.Module):
+    def __init__(self, idf, cdf, size):
+        super(ChannelAttention, self).__init__()
+        self.conv_context = conv1x1(cdf, size * size)
+        self.sm = nn.Softmax()
+        self.idf = idf
+
+    def forward(self, weightedContext, context, ih, iw):
+        batch_size, sourceL = context.size(0), context.size(2)
+        sourceC = context.unsqueeze(3)
+        sourceC = self.conv_context(sourceC).squeeze(3)
+        attn_c = torch.bmm(weightedContext, sourceC)
+        attn_c = attn_c.view(batch_size * self.idf, sourceL)
+        attn_c = self.sm(attn_c)
+        attn_c = attn_c.view(batch_size, self.idf, sourceL)
+
+        attn_c = torch.transpose(attn_c, 1, 2).contiguous()
+        weightedContext_T = torch.bmm(sourceC, attn_c)
+        weightedContext_T = torch.transpose(weightedContext_T, 1, 2).contiguous()
+        weightedContext_T = weightedContext_T.view(batch_size, -1, ih, iw)
+
+        return weightedContext_T, attn_c
+
+
+class EMA(nn.Module):
+    def __init__(self, channels, c2=None, factor=32):
+        super(EMA, self).__init__()
+        self.groups = factor
+        assert channels // self.groups > 0
+        self.softmax = nn.Softmax(-1)
+        self.agp = nn.AdaptiveAvgPool2d((1, 1))
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+        self.gn = nn.GroupNorm(channels // self.groups, channels // self.groups)
+        self.conv1x1 = nn.Conv2d(channels // self.groups, channels // self.groups, kernel_size=1, stride=1, padding=0)
+        self.conv3x3 = nn.Conv2d(channels // self.groups, channels // self.groups, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+        group_x = x.reshape(b * self.groups, -1, h, w)  # b*g,c//g,h,w
+        x_h = self.pool_h(group_x)
+        x_w = self.pool_w(group_x).permute(0, 1, 3, 2)
+        hw = self.conv1x1(torch.cat([x_h, x_w], dim=2))
+        x_h, x_w = torch.split(hw, [h, w], dim=2)
+        x1 = self.gn(group_x * x_h.sigmoid() * x_w.permute(0, 1, 3, 2).sigmoid())
+        x2 = self.conv3x3(group_x)
+        x11 = self.softmax(self.agp(x1).reshape(b * self.groups, -1, 1).permute(0, 2, 1))
+        x12 = x2.reshape(b * self.groups, c // self.groups, -1)  # b*g, c//g, hw
+        x21 = self.softmax(self.agp(x2).reshape(b * self.groups, -1, 1).permute(0, 2, 1))
+        x22 = x1.reshape(b * self.groups, c // self.groups, -1)  # b*g, c//g, hw
+        weights = (torch.matmul(x11, x12) + torch.matmul(x21, x22)).reshape(b * self.groups, 1, h, w)
+        return (group_x * weights.sigmoid()).reshape(b, c, h, w)
+
+
+class SpatialAttention_n(nn.Module):
+    def __init__(self):
+        super(SpatialAttention_n, self).__init__()
+        self.sa = nn.Conv2d(2, 1, 7, padding=3, padding_mode='reflect', bias=True)
+
+    def forward(self, x):
+        x_avg = torch.mean(x, dim=1, keepdim=True)
+        x_max, _ = torch.max(x, dim=1, keepdim=True)
+        x2 = torch.concat([x_avg, x_max], dim=1)
+        sattn = self.sa(x2)
+        return sattn
+
+
+class ChannelAttention_n(nn.Module):
+    def __init__(self, dim, reduction=8):
+        super(ChannelAttention_n, self).__init__()
+        self.gap = nn.AdaptiveAvgPool2d(1)
+        self.ca = nn.Sequential(
+            nn.Conv2d(dim, dim // reduction, 1, padding=0, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(dim // reduction, dim, 1, padding=0, bias=True),
+        )
+
+    def forward(self, x):
+        x_gap = self.gap(x)
+        cattn = self.ca(x_gap)
+        return cattn
+
+
+class PixelAttention(nn.Module):
+    def __init__(self, dim):
+        super(PixelAttention, self).__init__()
+        self.pa2 = nn.Conv2d(2 * dim, dim, 7, padding=3, padding_mode='reflect', groups=dim, bias=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x, pattn1):
+        B, C, H, W = x.shape
+        x = x.unsqueeze(dim=2)  # B, C, 1, H, W
+        pattn1 = pattn1.unsqueeze(dim=2)  # B, C, 1, H, W
+        x2 = torch.cat([x, pattn1], dim=2)  # B, C, 2, H, W
+        x2 = Rearrange('b c t h w -> b (c t) h w')(x2)
+        pattn2 = self.pa2(x2)
+        pattn2 = self.sigmoid(pattn2)
+        return pattn2
